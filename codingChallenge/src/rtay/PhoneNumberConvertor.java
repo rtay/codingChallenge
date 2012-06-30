285118d4f17e429a0f04b5d31f1ee133c68a8d5f
Raymond Tay <raymond_tay@y7mail.com>
2012-06-30 21:27:48 +1000

    first git test commit

---
+++ codingChallenge/src/rtay/PhoneNumberConvertor.java
@@ -0,0 +1,14 @@
+package rtay;
+
+public class PhoneNumberConvertor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1-800-CODING-CHALLENGE git test");
+		
+	}
+
+}
